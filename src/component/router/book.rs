8519d304e69276277e99f130fecc6621ff7531fe
waitsalt <waitsalt@outlook.com>
2024-11-04 13:27:58 +0800

    fixed something

--- src/component/router/book.rs
+++ src/component/router/book.rs
@@ -1,9 +1,32 @@
-use axum::{routing::get, Router};
+use axum::{
+    routing::{get, post},
+    Router,
+};
 
 pub async fn init() -> Router {
-    Router::new().route("/", get(book))
+    Router::new()
+        .route("/", get(book))
+        .route("/upload", post(upload_book))
+        .route("/book_id", get(read_book).put(edit_book))
+        .route("/book_id/chapter_id", get(read_chapter).put(edit_chapter))
+        .route("/download", get(download_book))
+        .route("/search", post(search_book))
 }
 
 pub async fn book() -> &'static str {
     "book"
 }
+
+async fn upload_book() {}
+
+async fn read_book() {}
+
+async fn edit_book() {}
+
+async fn read_chapter() {}
+
+async fn edit_chapter() {}
+
+async fn download_book() {}
+
+async fn search_book() {}
--- src/component/router/mod.rs
+++ src/component/router/mod.rs
@@ -1,4 +1,4 @@
-use axum::Router;
+use axum::{routing::get, Router};
 use tower_http::trace;
 
 pub mod book;
@@ -6,10 +6,17 @@ pub mod book;
 pub async fn init() -> Router {
     let book_router = book::init().await;
 
-    Router::new().nest("/book", book_router).layer(
-        trace::TraceLayer::new_for_http()
-            .make_span_with(trace::DefaultMakeSpan::new().include_headers(true))
-            .on_request(trace::DefaultOnRequest::new().level(tracing::Level::INFO))
-            .on_response(trace::DefaultOnResponse::new().level(tracing::Level::INFO)),
-    )
+    Router::new()
+        .nest("/book", book_router)
+        .fallback(get(fallback))
+        .layer(
+            trace::TraceLayer::new_for_http()
+                .make_span_with(trace::DefaultMakeSpan::new().include_headers(true))
+                .on_request(trace::DefaultOnRequest::new().level(tracing::Level::INFO))
+                .on_response(trace::DefaultOnResponse::new().level(tracing::Level::INFO)),
+        )
+}
+
+async fn fallback() -> &'static str {
+    "not found"
 }
